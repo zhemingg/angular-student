2041ac3af73adeaa33a96629cdf3a6d1b0738c1b
ZhemingGao <gao19911026@gmail.com>
2018-07-09 13:37:43 -0700

    created course component and homePage component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { CourseComponent } from './course/course.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    CourseComponent
   ],
   imports: [
     BrowserModule
