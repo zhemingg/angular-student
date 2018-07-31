59efb91f003c303cc6c862af1214f9f9627c2c3f
ZhemingGao <gao19911026@gmail.com>
2018-07-30 20:53:25 -0700

    update-version-2

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/angular-student-course-manger'));
 
+
 app.get('/*', function(req,res) {
 
   res.sendFile(path.join(__dirname+'/dist/angular-student-course-manger/index.html'));
